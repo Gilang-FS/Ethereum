b2a1cecc0dc968f7d98dff716a753df3fe0ccd8d
Gilang-FS <gilangprayudha160@gmail.com>
2026-03-17 20:48:42 +0700

    add files ke quiz/part2

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,18 @@
+let word = "wow JavaScript is so cool";
+let FirstWord = word.substring(0, 3);
+let SecondWord = word.substring(4, 14);
+let ThirdWord = word.substring(15, 17);
+let FourthWord = word.substring(18, 20);
+let FifthWord = word.substring(21, 25);
+
+let FirstWordLength = FirstWord.length;
+let SecondWordLength = SecondWord.length;
+let ThirdWordLength = ThirdWord.length;
+let FourthWordLength = FourthWord.length;
+let FifthWordLength = FifthWord.length;
+
+console.log("First word : " + FirstWord);
+console.log("Second word : " + SecondWord);
+console.log("Third word : " + ThirdWord);
+console.log("Fourth word : " + FourthWord);
+console.log("Fifth word : " + FifthWord);
