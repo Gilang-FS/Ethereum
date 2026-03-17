a2cbc6bdf385cde57bc31aa8cff5dd99dcc68988
Gilang-FS <gilangprayudha160@gmail.com>
2026-03-17 21:01:24 +0700

    soal 1

--- quiz/part3/soal1.js
+++ quiz/part3/soal1.js
@@ -1,9 +1,9 @@
-console.log("LOOPING PERTAMA");
+console.log("LOOPING MAJU\n");
 for (let i = 1; i <= 5; i++) {
   console.log(i);
 }
 
-console.log("LOOPING KEDUA");
+console.log("\nLOOPING MUNDUR\n");
 for (let i = 5; i >= 1; i--) {
   console.log(i);
 }
