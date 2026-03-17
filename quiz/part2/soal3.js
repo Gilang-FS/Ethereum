10d9c9d5c19d4d1e1a48bf81d536c60816511174
Gilang-FS <gilangprayudha160@gmail.com>
2026-03-17 20:35:39 +0700

    soal 3

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,12 @@
+let word = "wow JavaScript is so cool";
+let FirstWord = word.substring(0, 3);
+let SecondWord = word.substring(4, 14);
+let ThirdWord = word.substring(15, 17);
+let FourthWord = word.substring(18, 20);
+let FifthWord = word.substring(21, 25);
+
+console.log("First word : " + FirstWord);
+console.log("Second word : " + SecondWord);
+console.log("Third word : " + ThirdWord);
+console.log("Fourth word : " + FourthWord);
+console.log("Fifth word : " + FifthWord);
