ea8c7520bb2bbc4e8608de694ea8b55fd3bfe273
Gilang-FS <gilangprayudha160@gmail.com>
2026-03-20 21:12:02 +0700

    selesaiii

---
+++ quiz/ujian/soal3.js
@@ -0,0 +1,20 @@
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut.
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+  let depan = ~~(menit / 60);
+  let belakang = menit % 60;
+
+  if (belakang < 10) {
+    return depan + ":0" + belakang;
+  } else return depan + ":" + belakang;
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
---
+++ quiz/ujian/soal4.js
@@ -0,0 +1,19 @@
+//   Problem
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string.
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+  let jumlahX = str.split("x").length - 1;
+  let jumlahO = str.split("o").length - 1;
+
+  if (jumlahX == jumlahO) {
+    return "true";
+  } else return "false";
+}
+
+// TEST CASES
+console.log(xo("xoxoxo")); // true
+console.log(xo("oxooxo")); // false
+console.log(xo("oxo")); // false
+console.log(xo("xxxooo")); // true
+console.log(xo("xoxooxxo")); // true
